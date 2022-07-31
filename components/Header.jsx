edd862991b4f1803e410f474bd3df374631c3741
Toma <thomasmarques911@gmail.com>
2022-07-31 05:45:35 +0200

    readme update

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ import { ConnectButton } from "web3uikit"
 export default function Header() {
     return (
         <div className="p-5 border-b-2 flex flex-row">
-            <h1 className="py-2 px-9 front-blog text-3xl">Decentralize lotterie</h1>
+            <h1 className="py-2 px-9 front-blog text-3xl">Decentralize lottery</h1>
             <div className="ml-auto py-2 px-4">
                 <ConnectButton />
             </div>
